4a5868e56eb4cfe250dfe655af143e145be7a80b
Lavan Thaya <46497911+lavanthaya@users.noreply.github.com>
2021-06-17 01:19:25 -0230

    Fixed pip install error

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "Installing Conda packages...!"
 conda install -c conda-forge cartopy
 
 echo "Installing pip packages...!"
-pip install seaborn tqdm netCDF Pillow==6.2.2 matplotlib
+pip install seaborn tqdm netCDF4 Pillow==6.2.2 matplotlib
 
 echo "Installing ArgoPy"
 pip install git+http://github.com/euroargodev/argopy.git@master
